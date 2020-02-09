e62984556174a54b63f375b6771783268609f996
Mitch Perez <perez15@live.missouristate.edu>
2020-02-09 11:03:33 -0600

    takes screenshot of webcam every 3 seconds

--- camera_stream/web_base.py
+++ camera_stream/web_base.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import time
 import traceback  # importing necessary modules (Mitch)
+import os
 from threading import Timer
 
 video = cv2.VideoCapture(0)
@@ -10,28 +11,32 @@ fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 
-def hello():
-    print("hello, world")
+text = ''
 
 
-img_counter = 0
-while True:
-    # after 30 seconds, "hello, world" will be printed
-    ret, frame = video.read()
-    cv2.imshow('frame', frame)  # show the frame
-    k = cv2.waitKey(1)
+def displayFrame():
+    img_counter = 0
+    beginTime = time.time()
+    path = 'ImageGathering/'
+    while True:
+        # after 30 seconds, "hello, world" will be printed
+        ret, frame = video.read()
+        cv2.imshow('frame', frame)  # show the frame
+        k = cv2.waitKey(1)
+        if time.time() - beginTime >= 3:  # if 3 seconds have elapsed since the last img's capture
+            img_name = path + "opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1  # assuring images are labeled differently....
+            beginTime = time.time()
 
-    if k % 256 == 32:
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+        if k % 256 == 27:  # if the user presses the 'esc' key, end the program
+            print('test')
+            video.release()
+            cv2.destroyAllWindows()
 
-    if k % 256 == 27:  # if the key pressed = 'q' break loop
-        video.release()
-        cv2.destroyAllWindows()
-        break
 
+displayFrame()
 
 video.release()
 out.release()
