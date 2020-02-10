42954c50430821428dec4142e7c1a93160cddd82
Mitch Perez <perez15@live.missouristate.edu>
2020-02-09 20:57:31 -0600

    thoroughly documenting code (requested by eric)

--- camera_stream/status_log.py
+++ camera_stream/status_log.py
@@ -5,16 +5,16 @@ current_time = datetime.now()
 
 def logStatus(status):
     current_time = str(datetime.now())
-    if status == 'opening':
+    if status == 'opening':  # if program is being opened, document that it's being opened
         file = open('logHistory.txt', 'a')
         file.write('=========================================\n')
         file.write('program opened at: ' + current_time + '\n')
         file.close()
-    elif status == 'img':
+    elif status == 'img':  # if an image is being taken, document that it's being taken
         file = open('logHistory.txt', 'a')
         file.write('img taken at: ' + current_time + '\n')
         file.close()
-    elif status == 'closing':
+    elif status == 'closing':  # if program is being closed, document that it's being closed
         file = open('logHistory.txt', 'a')
         file.write('program closed at: ' + current_time + '\n')
         file.close()
--- camera_stream/web_base.py
+++ camera_stream/web_base.py
@@ -7,34 +7,39 @@ from status_log import *
 
 video = cv2.VideoCapture(0)  # create video object
 # recorded video is created into avi files
+# video file types (optional, if we ever need video for some reason)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avid', fourcc, 20.0, (640, 480))
+out = cv2.VideoWriter('output.avid', fourcc, 20.0,
+                      (640, 480))  # size of screen
 
 text = ''
 
 
 def displayFrame():
     print('opening program')
-    logStatus('opening')
+    logStatus('opening')  # documenting that the program was opened
     img_counter = 0
     beginTime = time.time()
-    path = 'ImageGathering/'
+    path = 'ImageGathering/'  # folder files are being saved to
     while True:
-        ret, frame = video.read()
-        cv2.imshow('frame', frame)
+        ret, frame = video.read()  # retrieving the video frame
+        cv2.imshow('frame', frame)  # displaying the frame
+        # if the user presses a key (i.e "escape" retrieve this action)
         k = cv2.waitKey(1)
+        # get the time to make every image name unique
         distinguishPath = str(time.time())
-        if time.time() - beginTime >= 3:
-            logStatus('img')
+        if time.time() - beginTime >= 3:  # if it's been 3 seconds since the last image was taken
+            logStatus('img')  # log that an image was taken
+            # format path name
             img_name = path + "opencv_frame_{}.png".format(distinguishPath)
             cv2.imwrite(img_name, frame)
             print("{} written!".format(img_name))
             beginTime = time.time()
-        if k % 256 == 27:
+        if k % 256 == 27:  # if escape if pressed, close the program
             print('closing program')
-            logStatus('closing')
-            video.release()
-            cv2.destroyAllWindows()
+            logStatus('closing')  # log that the program was closed
+            video.release()  # release video frames
+            cv2.destroyAllWindows()  # close window
 
 
 displayFrame()
