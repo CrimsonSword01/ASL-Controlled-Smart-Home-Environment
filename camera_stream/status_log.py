3b02af37d048656eb64a4f521b9f3a889120c632
Mitch Perez <perez15@live.missouristate.edu>
2020-02-09 20:35:53 -0600

    adding log files to monitor user activity

---
+++ camera_stream/status_log.py
@@ -0,0 +1,21 @@
+import time
+from datetime import datetime
+current_time = datetime.now()
+
+
+def logStatus(status):
+    current_time = str(datetime.now())
+    if status == 'opening':
+        file = open('logHistory.txt', 'a')
+        file.write('program opened at: ' + current_time + '\n')
+        file.close()
+    elif status == 'img':
+        file = open('logHistory.txt', 'a')
+        file.write('img taken at: ' + current_time + '\n')
+        file.close()
+    elif status == 'closing':
+        file = open('logHistory.txt', 'a')
+        file.write('program closed at: ' + current_time + '\n')
+        file.close()
+    else:
+        print('invalid input')
--- camera_stream/web_base.py
+++ camera_stream/web_base.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as numpy
 import time
 import traceback
+from status_log import *
 
 video = cv2.VideoCapture(0)  # create video object
 # recorded video is created into avi files
@@ -14,6 +15,7 @@ text = ''
 
 def displayFrame():
     print('opening program')
+    logStatus('opening')
     img_counter = 0
     beginTime = time.time()
     path = 'ImageGathering/'
@@ -23,13 +25,14 @@ def displayFrame():
         k = cv2.waitKey(1)
         distinguishPath = str(time.time())
         if time.time() - beginTime >= 3:
+            logStatus('img')
             img_name = path + "opencv_frame_{}.png".format(distinguishPath)
             cv2.imwrite(img_name, frame)
             print("{} written!".format(img_name))
             beginTime = time.time()
-
         if k % 256 == 27:
             print('closing program')
+            logStatus('closing')
             video.release()
             cv2.destroyAllWindows()
 
