8e9cadbddab9a314ab827109049f840dfa3d1fdb
Mitch Perez <perez15@live.missouristate.edu>
2020-02-13 22:45:56 -0600

    changing the log's documentation of pics, also removing the interval for image capture

--- camera_stream/status_log.py
+++ camera_stream/status_log.py
@@ -3,20 +3,16 @@ from datetime import datetime
 current_time = datetime.now()
 
 
-def logStatus(status):
+def logStatus(status, numImgs):
     current_time = str(datetime.now())
     if status == 'opening':  # if program is being opened, document that it's being opened
         file = open('logHistory.txt', 'a')
         file.write('=========================================\n')
         file.write('program opened at: ' + current_time + '\n')
         file.close()
-    elif status == 'img':  # if an image is being taken, document that it's being taken
-        file = open('logHistory.txt', 'a')
-        file.write('img taken at: ' + current_time + '\n')
-        file.close()
     elif status == 'closing':  # if program is being closed, document that it's being closed
         file = open('logHistory.txt', 'a')
-        file.write('program closed at: ' + current_time + '\n')
+        file.write('program closed at: ' + current_time + ', ' + str(numImgs) +' images recorded'+ '\n')
         file.close()
     elif status == 'clear':
         file = open('logHistory.txt', 'w')
--- camera_stream/web_base.py
+++ camera_stream/web_base.py
@@ -3,20 +3,16 @@
 # EDIT BY PAUL DURHAM - SEE EDIT 1 - 2/10/2020
 import time
 import cv2
-import numpy as numpy
+import numpy as np
 import traceback
 from status_log import *
-# from rec_test import checkGesture
+import keyboard
 import pathlib
 import sys
 my_path = pathlib.Path('rec_test').parent.absolute()
 my_path = str(my_path) + '/model_handler'
 print(my_path)
 sys.path.insert(0, my_path)
-
-# import rec_test
-
-
 video = cv2.VideoCapture(1)  # create video object
 # recorded video is created into avi files
 # video file types (optional, if we ever need video for some reason)
@@ -26,11 +22,10 @@ out = cv2.VideoWriter('output.avid', fourcc, 20.0,
 
 text = ''
 
-
 def displayFrame():
     print('opening program')
-    logStatus('opening')  # documenting that the program was opened
-    img_counter = 0
+    logStatus('opening', 0)  # documenting that the program was opened
+    imgCount = 0
     beginTime = time.time()
     # EDIT 1
     # EDIT BY PAUL DURHAM ON 2/10/2020
@@ -43,22 +38,23 @@ def displayFrame():
         k = cv2.waitKey(1)
         # get the time to make every image name unique
         distinguishPath = str(time.time())
-        if time.time() - beginTime >= 3:  # if it's been 3 seconds since the last image was taken
-            logStatus('img')  # log that an image was taken
-            # checkGesture(frame) #INTEGRATED w/ rec_test.py, checking for valid gestures
-            # format path name
-            img_name = path + "opencv_frame_{}.png".format(distinguishPath)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-            beginTime = time.time()
+        # if time.time() - beginTime >= 3:  # if it's been 3 seconds since the last image was taken
+        imgCount = imgCount + 1 # log that an image was taken
+        # checkGesture(frame) #INTEGRATED w/ rec_test.py, checking for valid gestures
+        # format path name
+        img_name = path + "opencv_frame_{}.png".format(distinguishPath)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img = cv2.imread(img_name)
+        beginTime = time.time()
         if k % 256 == 27:  # if escape if pressed, close the program
             print('closing program')
-            logStatus('closing')  # log that the program was closed
+            logStatus('closing', imgCount)  # log that the program was closed
             video.release()  # release video frames
             cv2.destroyAllWindows()  # close window
         if k % 256 == ord('c'):
             print('clearing log history')
-            logStatus('clear')
+            logStatus('clear', 0)
 
 
 displayFrame()
