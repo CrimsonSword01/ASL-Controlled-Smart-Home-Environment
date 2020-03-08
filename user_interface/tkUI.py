9d01f88e5865459a5d35856ba799d68740077ce6
unknown <omniyamagdi@gmail.com>
2020-03-08 14:52:41 -0500

    Updating GUI

--- user_interface/tkUI.py
+++ user_interface/tkUI.py
@@ -1,65 +1,67 @@
 import webbrowser
-import cv2
 from tkinter import *
-from PIL import Image, ImageTk
-#from PIL import ImageTk
 import time
-try:
-    from tkinter import ttk   
-except ImportError:
-    from tkinter import ttk
+from PIL import ImageTk, Image
+from tkinter import Label
+from tkinter import LabelFrame
+from tkinter import Frame
+from tkinter import ttk
 
 self = Tk()
 
+#turned into a class by Allison
 class SLISH(Frame):
     def __init__(self):
+
         Frame.__init__(self)
+        #parent frame 
         self.master.title('SLISH')
-        self.master.config(bg='#c9e4ff')
-
-        #picture didn't work without the full path - will change later
-        photo = PhotoImage(file='/Users/ANDor/OneDrive/Desktop/pic.png')
-        label = Label(self.master, image=photo)
-        label.pack()
+        mainframe= Frame(self.master, bg='#c9e4ff')
+        mainframe.pack(fill='both', expand=True)
+        #Full screen window 
+        self.master.geometry("{0}x{1}+1+1".format(self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
         
-        #centering the page- draft
-        window_height = 900
-        window_width = 1000
-
-        screen_width = self.master.winfo_screenwidth()
-        screen_height = self.master.winfo_screenheight()
-
-        x_cordinate = int((screen_width/2) - (window_width/2))
-        y_cordinate = int((screen_height/2) - (window_height/2))
-
-        self.master.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+        # displaying the title/logo of SLISH
+        header_frame= Frame(mainframe,bg='#c9e4ff')
+        header_frame.pack(fill='x')
+        photo = ImageTk.PhotoImage(Image.open('pic.png'))
+        label = Label(header_frame, image=photo)
+        label.image= photo
+        label.pack(padx=5, pady=5)
+        
+        # horizontal frame for camera stream and log
+        center_frame= Frame(mainframe)
+        center_frame.pack(fill='both')
+        center_frame.grid_columnconfigure(0,weight=1)
+        center_frame.grid_columnconfigure(1,weight=1)
 
         #space for camera_stream
-        camera_frame= LabelFrame(self.master, text='Stream:', padx=10, pady=10,bg='#f0f4fa')
-        camera_frame.pack(padx=10, pady=10,expand='yes', fill='both')
-
-        btn2 =ttk.Button(camera_frame, text='camera space to be filled later')
-        btn2.grid(row=0, column=0, padx=40, pady=20,sticky='w')
+        camera_frame= Frame(center_frame, padx=5, pady=5, borderwidth=2,bg='#c9e4ff')
+        camera_frame.grid(row=0,column=0, sticky='nsew')
+        #fill in code for video stream
+        btn2 =Button(camera_frame, text='camera space to be filled later',padx=1, pady=1, width=40, height=35)
+        btn2.pack( padx=2, pady=2, fill='both')
 
         #space for log
-        log_frame= LabelFrame(self.master, text='Log:', padx=10, pady=10,bg='#f0f4fa')
-        log_frame.pack(padx=10, pady=10,expand='yes', fill='both')
+        log_frame= Frame(center_frame, padx=5, pady=5, borderwidth=2,bg='#c9e4ff')
+        log_frame.grid(row=0,column=1, sticky='nsew')
+        #fill in code 
+        btn3 =Button(log_frame, text='log space to be filled later', width=40, height=20)
+        btn3.pack(padx=2, pady=2, fill='both')
 
-        btn3 =ttk.Button(log_frame, text='log space to be filled later')
-        btn3.grid(row=0, column=0, padx=40, pady=20,sticky='w')
-
-        # function that opens the link
+        # function that opens the help documentation link
         def open_help():
             webbrowser.open('https://github.com/mjp1997/ASL-Controlled-Smart-Home-Environment/blob/master/help.txt')
-
-        #button container
-        button_frame= LabelFrame(self.master, text='Help Documentation:', padx=20, pady=20,bg='#f0f4fa')
-        button_frame.pack(padx=10, pady=10, side='bottom')
+        
+        #FPS space
+        btn4 =Button(log_frame, text='FPS space to be filled later',padx=20, pady=10)
+        btn4.pack(padx=10, pady=10, side='left')
 
         # button that calls open_help()
-        btn =ttk.Button(button_frame, text='Help', command=open_help)
-        btn.grid(row=0, column=0, padx=10, pady=10,sticky='w')
-        #btn.pack(side="bottom", fill="both", expand="yes", padx=10,pady=10)
+        btn =ttk.Button(log_frame, text='HELP', command=open_help)
+        btn.pack(padx=20, pady=20, side='right')
+
+        
 
 
 def main():
